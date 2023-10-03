bcc9138260bf85255a2835953ec3405f6e2a9ffd
Balesabu Godugu <godugubalesabu@gmail.com>
2023-10-03 11:31:58 +0000

    added root module changes

--- main.tf
+++ main.tf
@@ -1,33 +1,5 @@
 
-terraform {
 
-   cloud {
-    organization = "practice_aws_azure"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-
-provider "aws" {
-  # Configuration options
-}
 
 resource "random_string" "bucket_name" {
   length           = 16
@@ -40,8 +12,8 @@ resource "random_string" "bucket_name" {
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
-}
+  tags = {
+    UserUuid = var.user_uuid
+  }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
 }
